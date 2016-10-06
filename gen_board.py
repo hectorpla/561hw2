e6dab28800e082bd856df3bf058caa7e063508b6
Zhanpeng Liang <hectorilles@gmail.com>
2016-10-06 15:04:21 -0700

    First commit

---
+++ gen_board.py
@@ -0,0 +1,236 @@
+import random, math, sys, argparse
+##### generate gang board HW2
+
+N = 5
+MODE = 'MINIMAX'
+# YOUPLAY = ''
+MAX_DEPTH = 5
+CELL_VALUES = []
+BOARD = []
+
+MAX_VAL = 100
+
+
+######################### randomize paras #########################
+N = random.randrange(26)
+MODE = random.choice(['MINIMAX', 'ALPHABETA']) # and 'COMPETITION'
+YOUPLAY = random.choice(['X', 'O'])
+MAX_DEPTH = int(N * N / 2)
+
+######################### parsing #########################
+DEBUG = False
+
+parser = argparse.ArgumentParser(description='Randomly generate gang board')
+parser.add_argument('-n', metavar = 'N', type=int, default='5')
+parser.add_argument('-d', action='store_true', default=False)
+args = parser.parse_args()
+N = args.n
+DEBUG = args.d
+# print (DEBUG)
+# quit()
+
+######################### initialize board info #########################
+CELL_VALUES = [[random.randrange(MAX_VAL) for j in range(N)] for i in range(N)]
+BOARD = [['.' for j in range(N)] for i in range(N)] 
+# dont use * N to declare a 2-d array, which copy the pointer of the first row
+
+######################### generate board utility #########################
+AVAILABLES = [] # might be outdated, because only updated when 50% 25%...
+NUM_AVAILABLES = 0
+UPDATE_POINT = 0
+OWNED_PIECES = {}
+
+def init():
+    global N, AVAILABLES, NUM_AVAILABLES, OWNED_PIECES, UPDATE_POINT
+    AVAILABLES = [x for x in range(N * N)]
+    NUM_AVAILABLES = N * N
+    OWNED_PIECES = {'X':set(), 'O':set()}
+    UPDATE_POINT = math.floor(N / 2)
+
+def update_avails():
+    global N, AVAILABLES, BOARD
+    AVAILABLES = []
+    for i in range(N):
+        for j in range(N):
+            if BOARD[i][j] == '.':
+                AVAILABLES.append(i * N + j)
+
+def decrease_availables():
+    global N, AVAILABLES, NUM_AVAILABLES, UPDATE_POINT
+    if NUM_AVAILABLES == 0:
+        print('To decrease NUM_AVAILABLES when it has been 0')
+        raise()
+    
+    NUM_AVAILABLES -= 1
+    if NUM_AVAILABLES == UPDATE_POINT:
+        update_avails()
+        UPDATE_POINT /= 2 #
+
+def get_opponent(player):
+    opponent = ''
+    if player == 'X':
+        opponent = 'O'
+    else:
+        opponent = 'X'
+    return opponent
+    
+
+######################### game operators #########################
+
+# randomly stake an unoccupied cell
+def random_stake(player):
+    global N, BOARD, OWNED_PIECES, DEBUG
+    
+    while (1):
+        r, c = random.randrange(N), random.randrange(N)
+        # print(N, r, c)
+#         print(BOARD)
+        if BOARD[r][c] == '.':
+            BOARD[r][c] = player
+            OWNED_PIECES[player].add(r * N + c)
+            decrease_availables() # not necessary in solver
+            if DEBUG:
+                print('staked:', r, c)
+            break
+
+# get the possible moves for raiding from cell (r, c)
+def get_raid_moves(r,c):
+    global BOARD, N
+    
+    moves = []
+    if r > 0:
+        if BOARD[r-1][c] == '.':
+            moves.append((-1, 0))
+    if r < N - 1:
+        if BOARD[r+1][c] == '.':
+            moves.append((1, 0))
+    if c > 0:
+        if BOARD[r][c-1] == '.':
+            moves.append((0, -1))
+    if c < N - 1:
+        if BOARD[r][c+1] == '.':
+            moves.append((0, 1))
+    return moves
+
+# change the ownership of cell (row, col) into player
+def change_ownership(player, opponent, row, col):
+    global N, BOARD, OWNED_PIECES, DEBUG
+    cell = row * N + col
+    BOARD[row][col] = player
+    OWNED_PIECES[opponent].remove(cell)
+    OWNED_PIECES[player].add(cell)
+    if DEBUG:
+        print('swallowed: ', row, col)
+
+# swallow the surrounding opponent tiles
+def swallow_around(player, r, c):
+    global N, BOARD
+    opponent = get_opponent(player)
+    if r > 0:
+        if BOARD[r-1][c] == opponent:
+            change_ownership(player, opponent, r-1, c)
+    if r < N - 1:
+        if BOARD[r+1][c] == opponent:
+            change_ownership(player, opponent, r+1, c)
+    if c > 0:
+        if BOARD[r][c-1] == opponent:
+            change_ownership(player, opponent, r, c-1)
+    if c < N - 1:
+        if BOARD[r][c+1] == opponent:
+            change_ownership(player, opponent, r, c+1)
+
+# raid from cell (row, col) using move
+def raid(player, row, col, move):
+    global N, BOARD, OWNED_PIECES, DEBUG
+    if (row * N + col) not in OWNED_PIECES[player]:
+        print('raid from a cell not from ' + player + '\'s territory')
+        raise
+    r = row + move[0]
+    c = col + move[1]
+    BOARD[r][c] = player
+    OWNED_PIECES[player].add(r * N + c)
+    decrease_availables() # not necessary in solver
+    if DEBUG:
+        print('raided from:', row, col, 'move:', move)
+    # print_board()
+    # occupy touching cells owned by enemy
+    swallow_around(player, r, c)    
+            
+# randomly raid from the piece owned by player
+def random_raid(player):
+    global N, OWNED_PIECES
+    
+    if len(OWNED_PIECES[player]) == 0:
+        return False            
+    # choose piece to raid from until success, at most 5 times
+    for i in range(5):
+        cell = random.sample(OWNED_PIECES[player], 1)[0]
+        r, c = int(cell / N), cell % N
+        moves = get_raid_moves(r, c)
+        if len(moves):
+            break
+    if len(moves) == 0: # no valid move after sampling five times
+        return False
+        
+    move = random.choice(moves) # choose a valid move    
+    raid(player, r, c, move)
+    return True
+    
+# randomly choose to stake or raid
+def random_play(player):
+    if random.random() < 0.5:
+        if random_raid(player):
+            return
+    random_stake(player)
+
+############################## other utilities ##############################
+def print_board():
+    global N, BOARD
+    for r in range(N):
+        for c in range(N):
+            print(BOARD[r][c], end = '')
+        print()
+    print()
+
+
+############################## main ##############################
+init()
+
+num_plays = random.randrange(N * N - 1)
+print('N: ' + str(N) + '\tnum_ply: ' + str(num_plays) + '\n')
+
+player = 'X'
+while ( num_plays ):
+    if DEBUG:
+        print(player + ':')
+    random_play(player)
+    if DEBUG:
+        print(OWNED_PIECES)
+        print_board()
+    player = get_opponent(player)
+    num_plays -= 1
+
+
+with open('input.txt', 'w') as f:
+    f.write(str(N) + '\n')
+    f.write(MODE + '\n')
+    f.write(player + '\n')
+    f.write(str(random.randrange(1, MAX_DEPTH + 1)) + '\n')
+    
+    for row in CELL_VALUES:
+        for cell in row:
+            print(str(cell) + ' ', end = '')
+        print()
+        s = ' '.join(repr(val) for val in row) + '\n'
+        f.write(s)
+    print()
+    
+    for r in range(N):
+        for c in range(N):
+            print(BOARD[r][c], end = '')
+            f.write(str(BOARD[r][c]))
+        print()
+        f.write('\n')
+    print()
+
+
---
+++ homework3.py
@@ -0,0 +1,297 @@
+# 561 hw2: minimax game playing
+import string
+
+N = 5
+MODE = ''
+YOUPLAY = ''
+CELL_VALUES = []
+DEPTH = 1
+
+# game state
+board = []
+AVAILABLES = set()
+OWNED_PIECES = {'X':set(), 'O':set()}
+
+######################### parsing #########################    
+def parse_values(value_lines):
+    """
+    :rtype: list (2-d) containg cell value
+    """
+    value_board = []
+    for line in value_lines:
+        value_board.append( list(map(int, line.split())) )
+    return value_board
+        
+def parse_board(board_lines):
+    """
+    :rtype: list (2-d containing ownership), int
+    """
+    global AVAILABLES, OWNED_PIECES
+    size = len(board_lines)
+    board = []
+    for r in range(size):
+        board.append([])
+        for c in range(size):
+            board[-1].append(board_lines[r][c])
+            if board_lines[r][c] is '.': AVAILABLES.add((r,c))
+            if board_lines[r][c] is 'X': OWNED_PIECES['X'].add((r,c))
+            if board_lines[r][c] is 'O': OWNED_PIECES['O'].add((r,c))
+            
+    return board
+
+######################### utility #########################
+def print_board(board, delim):
+    size = len(board)
+    
+    for r in range(size):
+        for c in range(size):
+            print(str(board[r][c]) + delim, end = '')
+        print()
+    print()
+ 
+def print_paras():
+    global N, MODE, YOUPLAY, DEPTH
+    print('N = ' + str(N) + '\nMODE = ' + MODE + '\nYOUPLAY = ' + YOUPLAY
+        + '\nDEPTH = ' + str(DEPTH) + '\n')
+
+def get_opponent(player):
+    opponent = ''
+    if player == 'X': opponent = 'O'
+    else: opponent = 'X'
+    return opponent
+    
+def compute_eval(board, player):
+    global N, CELL_VALUES
+    
+    opponent = get_opponent(player)
+    res = 0
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] is player: res += CELL_VALUES[r][c]
+            if board[r][c] is opponent: res -= CELL_VALUES[r][c]
+    return res
+
+######################### game operators #########################
+# stake a position and return the gain
+def stake(board, player, pos):
+    global N, CELL_VALUES, AVAILABLES
+    print(player + ' staking ' + str(pos)) ## print
+    r, c = pos[0], pos[1]
+    assert board[r][c] is '.'
+    board[r][c] = player
+    AVAILABLES.remove(pos)
+    OWNED_PIECES[player].add(pos)
+    return CELL_VALUES[r][c]
+    
+def remove_ownership(board, player, pos):
+    global N, AVAILABLES, OWNED_PIECES
+    print(player + ' revert stake/raid ' + str(pos)) ## print
+    r, c = pos[0], pos[1]
+    assert board[r][c] != '.'
+    board[r][c] = '.'
+    OWNED_PIECES[player].remove(pos)
+    AVAILABLES.add(pos)
+
+# change the ownership of pos into player, return the gain
+def change_ownership(board, player, opponent, r, c):
+    global OWNED_PIECES
+    assert board[r][c] is opponent
+    board[r][c] = player
+    OWNED_PIECES[opponent].remove((r,c))
+    OWNED_PIECES[player].add((r,c))
+    return 2 * CELL_VALUES[r][c]
+
+
+# swallow the surrounding opponent tiles, return the gain
+def swallow_around(board, player, pos):
+    global N, OWNED_PIECES
+    opponent = get_opponent(player)
+    assert pos in OWNED_PIECES[player]
+    r, c = pos[0], pos[1]
+    
+    gain = 0
+    swallowed = []
+    if r > 0 and board[r-1][c] == opponent:
+        gain += change_ownership(board, player, opponent, r-1, c)
+        swallowed.append((r-1, c))
+    if r < N - 1 and board[r+1][c] == opponent:
+        gain += change_ownership(board, player, opponent, r+1, c)
+        swallowed.append((r+1, c))
+    if c > 0 and board[r][c-1] == opponent:
+        gain += change_ownership(board, player, opponent, r, c-1)
+        swallowed.append((r, c-1))
+    if c < N - 1 and board[r][c+1] == opponent:
+        gain += change_ownership(board, player, opponent, r, c+1)
+        swallowed.append((r, c+1))
+    print(player + ' swallowed: ' + str(swallowed))
+    return gain, swallowed
+
+# return all the possible raid moves from pos
+def get_raid_moves(board, pos):
+    """
+    : rtype: lists of two tuple (row_move, col_move)
+    """
+    global N
+    moves = []
+    r, c = pos[0], pos[1]
+    if r > 0 and board[r-1][c] == '.':
+        moves.append((-1, 0))
+    if r < N - 1 and board[r+1][c] == '.':
+            moves.append((1, 0))
+    if c > 0 and board[r][c-1] == '.':
+            moves.append((0, -1))
+    if c < N - 1 and board[r][c+1] == '.':
+            moves.append((0, 1))
+    return moves
+
+# raid from pos using move, return the gain
+def raid(board, player, pos, move):
+    global N, CELL_VALUES
+    print(player + ' raiding from:' + str(pos) + ' move: ' + str(move)) ## print
+    assert pos in OWNED_PIECES[player]
+    r, c = pos[0] + move[0], pos[1] + move[1]
+    assert board[r][c] is '.'
+    board[r][c] = player
+    AVAILABLES.remove((r,c))
+    OWNED_PIECES[player].add((r,c))
+    raid_gain = CELL_VALUES[r][c]
+    # occupy touching cells owned by enemy
+    swallow_gain, swallowed = swallow_around(board, player, (r,c))
+    return raid_gain + swallow_gain, swallowed
+
+# get the state of 
+def revert_swallow(board, swallowed, player, opponent):
+    global OWNED_PIECES
+    for s in swallowed:
+        r, c = s[0], s[1]
+        assert board[r][c] is player
+        board[r][c] = opponent
+        OWNED_PIECES[player].remove(s)
+        OWNED_PIECES[opponent].add(s)
+    print(player + ' revert swallow: ' + str(swallowed)) ## print
+    
+
+######################## algos #########################
+def MiniMax(board, player, depth, eval):
+    """
+    : return the minimax decision
+    """
+    global CELL_VALUES, DEPTH, AVAILABLES, OWNED_PIECES
+    move_type = 'Stake'
+    move_target = (0, 0) # ok?
+    
+    max_val = float('-inf')
+    opponent = get_opponent(player)    
+    # stake
+    for tile in AVAILABLES:
+        change_eval = stake(board, player, tile)
+        new_eval = eval + change_eval
+        print(player + " new eval: " + str(new_eval)) ## print        
+        temp = Min_Value(board, opponent, depth + 1, new_eval) # compute 
+        if temp > max_val:
+            max_val = temp
+            move_target = tile
+        print(player + " min value: " + str(temp)) ## print
+        # restore the original state
+        remove_ownership(board, player, tile)
+        print()
+    # raid
+    for tile in OWNED_PIECES[player]:
+        moves = get_raid_moves(board, tile)
+        print(player + "'s rading moves" + str(moves)) ## print
+        for move in moves:            
+            change_eval, swallowed = raid(board, player, tile, move)
+            new_eval = eval + change_eval
+            raided = (tile[0] + move[0], tile[1] + move[1])
+            temp = Min_Value(board, opponent, depth + 1, new_eval) # compute
+            if temp > max_val:
+                max_val = temp
+                move_type = 'Raid'
+                move_target = raided
+            print(player + " min value: " + str(temp)) ## print
+            # restore the original state
+            # print_board(board, '')
+#             print(swallowed)
+            revert_swallow(board, swallowed, player, opponent)
+            remove_ownership(board, player, raided)
+        
+    return move_target, move_type
+       
+def Max_Value(board, player, depth, last_eval):
+    global CELL_VALUES, DEPTH, AVAILABLES, OWNED_PIECES
+    
+    print('Max_Value: depth ' + str(depth) + ' DEPTH: ' + str(DEPTH))
+    if len(AVAILABLES) == 0 or depth == DEPTH: 
+        return last_eval # should simply call compute function?
+    
+    max_val = float('-inf')
+    opponent = get_opponent(player)
+    # stake
+    for tile in AVAILABLES:
+        change_eval = stake(board, player, tile)
+        new_eval = last_eval + change_eval
+        max_val = max(max_val, Min_Value(board, opponent, depth + 1, new_eval)) #
+        remove_ownership(board, player, tile)
+    # raid
+    for tile in OWNED_PIECES[player]:
+        moves = get_raid_moves(board, tile)
+        for move in moves:            
+            change_eval, swallowed = raid(board, player, tile, move)
+            new_eval = last_eval + change_eval
+            max_val = max(max_val, Min_Value(board, opponent, depth + 1, new_eval))
+            revert_swallow(board, swallowed, player, opponent)
+            raided = (tile[0] + move[0], tile[1] + move[1])
+            remove_ownership(board, player, raided)
+        
+    return max_val
+    
+def Min_Value(board, player, depth, last_eval):
+    global CELL_VALUES, DEPTH, AVAILABLES, OWNED_PIECES
+    
+    print('Min_Value: depth ' + str(depth) + ' DEPTH: ' + str(DEPTH))
+    if len(AVAILABLES) == 0 or depth == DEPTH: 
+        return last_eval # should simply call compute function?
+    
+    min_val = float('inf')
+    opponent = get_opponent(player)
+    # stake
+    for tile in AVAILABLES:
+        change_eval = stake(board, player, tile)
+        new_eval = last_eval - change_eval
+        min_val = min(min_val, Max_Value(board, opponent, depth + 1, new_eval)) #
+        remove_ownership(board, player, tile)
+    # raid
+    for tile in OWNED_PIECES[player]:
+        moves = get_raid_moves(board, tile)
+        for move in moves:            
+            change_eval, swallowed = raid(board, player, tile, move)
+            new_eval = last_eval - change_eval
+            min_val = min(min_val, Max_Value(board, opponent, depth + 1, new_eval))
+            revert_swallow(board, swallowed, player, opponent)
+            raided = (tile[0] + move[0], tile[1] + move[1])
+            remove_ownership(board, player, raided)
+        
+    return min_val
+    
+######################### main #########################
+with open('input.txt', 'r') as f:
+    lines = f.read().splitlines()
+    # print(mylist)
+
+N = int(lines[0])
+MODE = lines[1]
+YOUPLAY = lines[2]
+DEPTH = int(lines[3])
+CELL_VALUES = parse_values(lines[4:4+N])
+board = parse_board(lines[4+N:4+2*N])
+
+print_paras()
+# print_board(CELL_VALUES, ' ')
+# print_board(board, '') 
+print(AVAILABLES) 
+print(OWNED_PIECES)
+
+eval = compute_eval(board, YOUPLAY)
+print('initial eval: ' + str(eval))
+type, cell = MiniMax(board, YOUPLAY, 0, eval)
+print(type, cell)
