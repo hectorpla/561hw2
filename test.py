58644c2c462c60ca423fc8c6d44251da9f2d0f46
Zhanpeng Liang <hectorilles@gmail.com>
2016-10-08 12:47:07 -0700

    before fixing tie breaking bug

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import os
 
-for f in range(10):
+list = [i for i in range(20)] + [i for i in range(30, 40)]
+
+for f in list:
     os.system('cp inputs/intput{0}.txt  input.txt'.format(f))
     os.system('python3 homework3.py')
-    input("PRESS ENTER TO CONTINUE.")
+    os.system('cp output.txt  outputs/output{0}.txt'.format(f))
+#     input("PRESS ENTER TO CONTINUE.")
